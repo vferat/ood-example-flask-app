787d0e7acfa494564b0b66e139d8ab643eeac134
Victor Férat <victor.ferat@live.Fr>
2024-02-19 17:10:14 +0100

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,9 @@
-from app import MyApp
 
-application = MyApp
 
 if __name__ == '__main__':
     import sys
     raise Exception(sys.executable)
+    from app import MyApp
+
+    application = MyApp
     application.run()

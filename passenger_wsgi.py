49380989df805f53c0409144b6979f90219ab1c2
Victor Férat <victor.ferat@live.Fr>
2024-02-19 17:07:48 +0100

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,8 @@
-#!/home/users/f/ferat/ondemand/dev/ood-example-flask-app/.venv/bin/python
-import sys
 from app import MyApp
 
 application = MyApp
 
 if __name__ == '__main__':
+    import sys
+    print(sys.executable)
     application.run()

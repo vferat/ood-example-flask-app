0bc1df1c9646ef1ca468dcba9ac3e6f2d19e8cd7
Victor Férat <victor.ferat@live.Fr>
2024-02-26 18:21:19 +0100

    wip

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from client.client import client_bp
 
 app = Flask(__name__)
 app.register_blueprint(api_bp, url_prefix='/api_v1')
-app.register_blueprint(client_bp, url_prefix='/client')
+app.register_blueprint(client_bp, url_prefix='')
 
 
 if __name__ == "__main__":

1289df1632839fb2db81799c2f13a525fb104122
Victor Férat <victor.ferat@live.Fr>
2024-02-26 18:10:26 +0100

    test

--- client/vue-app/src/App.vue
+++ client/vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import FmriprepForm from './components/FmriprepForm.vue';
 
-const apiEndpoint = '/api_v1/';
+const apiEndpoint = 'pun/dev/ood-example-flask-app/api_v1/';
 
 export default {
   name: 'App',
